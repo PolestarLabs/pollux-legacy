fd239ab58f507e4b1f0ba84813a7ceb249750195
Lucas Flicky @ Falk <lucasflicky@hotmail.com>
2017-11-01 17:53:50 -0200

    gearbox: schemas methods

--- core/gearbox.js
+++ core/gearbox.js
@@ -1,3 +1,32 @@
+const fs=require('fs');
+
+//const mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/politest', { useMongoClient: true });
+//mongoose.Promise = global.Promise;
+
+
+const Schemas = require('./schemas.js');
+
+const userDB    = Schemas.user;
+const serverDB  = Schemas.server;
+const channelDB = Schemas.channel;
+
+userDB.new = function(obj){
+  let instance = new userDB;
+  instance.id=obj.id;
+  instance.tag=obj.tag;
+  instance.name=obj.username;
+  instance.save();
+};
+
+userDB.set = function(query,alter){
+  if(['string','number'].includes(typeof query)){
+    query = {'id':query.toString()};
+  };
+  if(!typeof alter) throw "Invalid Alter Object";
+  return userDB.findOneAndUpdate(query,alter);
+};
+
 
 
 
@@ -7,11 +36,11 @@ module.exports={
 
   getDirs : function getDirs(rootDir, cb) {
     fs.readdir(rootDir, function (err, files) {
-        var dirs = [];
-        for (var i = 0; i < files.length; ++i) {
-            var file = files[i];
+        let dirs = [];
+        for (let i = 0; i < files.length; ++i) {
+            let file = files[i];
             if (file[0] !== '.') {
-                var filePath = rootDir + '/' + file;
+                let filePath = rootDir + '/' + file;
                 fs.stat(filePath, function (err, stat) {
                     if (stat.isDirectory()) {
                         dirs.push(this.file);
