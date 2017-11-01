493331e19ab57d476695d46a691cdc11c333d203
Lucas Flicky @ Falk <lucasflicky@hotmail.com>
2017-11-01 15:41:52 -0200

    new gearbox

---
+++ core/gearbox.js
@@ -0,0 +1,40 @@
+
+
+
+module.exports={
+
+
+
+  getDirs : function getDirs(rootDir, cb) {
+    fs.readdir(rootDir, function (err, files) {
+        var dirs = [];
+        for (var i = 0; i < files.length; ++i) {
+            var file = files[i];
+            if (file[0] !== '.') {
+                var filePath = rootDir + '/' + file;
+                fs.stat(filePath, function (err, stat) {
+                    if (stat.isDirectory()) {
+                        dirs.push(this.file);
+                    }
+                    if (files.length === (this.index + 1)) {
+                        return cb(dirs);
+                    }
+                }.bind({
+                    index: i,
+                    file: file
+                }));
+            }
+        }
+    })
+ },
+
+
+
+
+
+
+
+
+
+
+}
