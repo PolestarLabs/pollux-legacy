97909540943fbc8cf3a7d6362ccb77ebfa92a459
Lucas Flicky @ Falk <lucasflicky@hotmail.com>
2017-11-02 04:47:29 -0200

    update commands

---
+++ core/modules/infra/invite.js
@@ -0,0 +1,11 @@
+const cmd = 'invite';
+const init = function (message,userDB,DB) {
+  message.reply("Invite me to your Server using this: <http://goo.gl/qkGqqU>")
+}
+ module.exports = {
+    pub:true,
+    cmd: cmd,
+    perms: 8,
+    init: init,
+    cat: 'infra'
+};
