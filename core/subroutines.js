ec7c927254e4b89c5a3ccc37c11c8641d613dd67
Lucas Flicky @ Falk <lucasflicky@hotmail.com>
2017-11-05 17:53:44 -0200

    subroutines Upfactor

--- core/subroutines.js
+++ core/subroutines.js
@@ -7,19 +7,21 @@ async function levelChecks(message,servData,userData) {
   let forNext = Math.trunc(Math.pow((userData.modules.level + 1) / _CURVE, 2));
 
   const thisGdata = servData.modules.LOCALRANK[message.author.id]||{exp:0,level:0};
-  const _FACTOR = 0.0872899 || servData.modules.UPFACTOR;
+  const _FACTOR =  servData.modules.UPFACTOR||0.0872899;
 
   let curLevel_local = Math.floor(_FACTOR * Math.sqrt(thisGdata.exp));
   let forNext_local = Math.trunc(Math.pow((thisGdata.level + 1) / _FACTOR, 2));
 
   if (curLevel_local > thisGdata.level) {
+    thisGdata.level=curLevel_local;
     await gear.serverDB.findOneAndUpdate({id: message.guild.id},{
       $set: {['modules.LOCALRANK.' + message.author.id]: thisGdata
       }
     });
   };
+
   await gear.userDB.set(message.author.id,{$inc:{'modules.exp':5}});
-  console.log({forNext,XP:userData.modules.exp,LV: userData.modules.level})
+  //console.log({forNext,XP:userData.modules.exp,LV: userData.modules.level})
   if (curLevel > userData.modules.level) {
     await gear.userDB.set(message.author.id,{$set:{'modules.level':curLevel}});
     let overallevel = (await gear.userDB.findOne({id: message.author.id})).modules.level;
