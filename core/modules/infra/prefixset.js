fda7280e2f2bf0e73ded62af70914525128f3f10
Lucas Flicky @ Falk <lucasflicky@hotmail.com>
2017-11-09 21:30:38 -0200

    prefix

---
+++ core/modules/infra/prefixset.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const gear = require("../../gearbox.js");
+const paths = require("../../paths.js");
+const locale = require('../../../utils/multilang_b');
+const mm = locale.getT();
+
+const cmd = 'prefixSet';
+
+const init = function (message,userDB,DB) {
+const args = message.content.split(' ').slice(1)
+    let Server = message.guild;
+    let LANG = message.lang;
+    let bot = message.botUser
+
+    gear.paramDefine(Server, 'PREFIX', args[0].toString());
+    Server.dDATA.modules.PREFIX =  args[0].toString();
+    message.reply(mm('CMD.prefixChng', {
+        lngs: LANG,
+        prefix: Server.dDATA.modules.PREFIX
+    }));
+}
+
+ module.exports = {
+    cmd: cmd,
+    perms: 2,
+    init: init,
+    cat: 'master'
+};
